e2e360543fb29ce7421da625c101b1c7dcf120d2
Ahmad Nibras CT <nibrasctkappil@gmail.com>
2026-04-12 14:11:10 +0530

    feat: added pagination to wallet and my order page

--- src/controllers/walletController.js
+++ src/controllers/walletController.js
@@ -7,7 +7,23 @@ exports.getWalletPage = async (req, res) => {
 
         wallet.transactions.sort((a, b) => b.date - a.date);
 
-        res.render('user/wallet', { wallet });
+        const page = parseInt(req.query.page) || 1;
+        const limit = 5;
+
+        const totalTransactions = wallet.transactions.length;
+        const totalPages = Math.ceil(totalTransactions / limit);
+
+        const startIndex = (page - 1) * limit;
+        const endIndex = startIndex + limit;
+
+        const paginatedTransactions = wallet.transactions.slice(startIndex, endIndex);
+
+        res.render('user/wallet', { 
+            walletBalance: wallet.balance,
+            transactions: paginatedTransactions,
+            currentPage: page,
+            totalPages: totalPages     
+        });
     } catch (error) {
         console.error("Wallet Page Error:", error);
         res.redirect('/profile?error=WalletError');
